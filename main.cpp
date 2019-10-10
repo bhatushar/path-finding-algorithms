dcc5cfdc3e09a6167b6e9fd73c15eac596ea16cf
Tushar <tbhatt321@gmail.com>
2019-10-10 22:48:48 +0530

    Empty main

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "include/Graph.h"
+
+// Prototypes
+Path dijkstra(const Graph& g, uint source, uint destination);
+Path aStar(const Graph& g, uint source, uint destination);
+
+int main() {
+    return 0;
+}
